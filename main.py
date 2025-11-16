b19fb3774c4005904b782df6ee13b5742ace9159
FleterEwenn <fleter.ewenn@gmail.com>
2025-11-16 18:04:01 +0100

    creation du fond

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ import subprocess
 import sys
 import pygame
 
-subprocess.Popen([sys.executable, "CheckPassword.py"])
-
 pygame.init()
 
-window = pygame.display.set_mode((1000, 600))
+window = pygame.display.set_mode((1536, 1024))
 
 run = True
+
+def run_checkpassword():
+    subprocess.Popen([sys.executable, "CheckPassword.py"])
+
+bgimage = pygame.image.load("bgimage.png")
 
 while run:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT :
             run = False
+    
+    window.blit(bgimage, (0, 0))
+
+    pygame.display.flip()
