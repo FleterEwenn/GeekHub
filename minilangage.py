a182eafb7b08f5db5e0e490f35400eef1c8c93d3
FleterEwenn <fleter.ewenn@gmail.com>
2025-12-20 19:50:32 +0100

    variable du mini langage

--- minilangage.py
+++ minilangage.py
@@ -9,8 +9,36 @@ def split_code():
             clean_lines.append(line.strip())
     executer_code(clean_lines)
 
-def executer_code(code:list):
-    print(code)
+def executer_code(code:list[str]):
+    var = {}
+
+    for line in code:
+        if line.startswith("spinjutzu"): # affichage dans la console
+            pass
+        elif line.startswith("nindroide "): # creation d'une variable
+            try:
+                current_line = line.split("=")
+                name = current_line[0][10:]
+                val = current_line[1]
+                val.join
+
+                for name in var.keys():
+                    if name in val:
+                        val = val.split(name)
+                        if type(var[name]) == str:
+                            tojoin = "'" + var[name] + "'"
+                        else:
+                            tojoin = str(var[name])
+                        
+                        val = tojoin.join(val)
+
+                var[name] = eval(val)
+            except Exception as e:
+                print(e)
+            except ValueError as e:
+                print(e)
+    
+    print(var)
 
 window = Tk()
 
@@ -20,7 +48,12 @@ window.geometry("800x600")
 zone_text = Text(window, width=80, height=20)
 execute_btn = Button(window, command=split_code, text="executer")
 
+console = Text(window, width=80, height=13)
+
 execute_btn.pack()
 zone_text.pack()
+
+console.pack()
+console.config(state=DISABLED)
 
 window.mainloop()
